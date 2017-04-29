b7495a2a84b58306a14a8c94eec6cc7c4d9dfe82
Matt De Bono <matthew.j.debono@gmail.com>
2017-04-29 22:00:08 +1000

    Add Tillybot class

---
+++ tillybot.py
@@ -0,0 +1,13 @@
+class Tillybot():
+
+	def __init__(self):
+		pass
+
+	def itillianise(self, message_text):
+		return text
+
+	def has_keywords(self, message_text):
+		pass
+
+	def respond(self, message_text):
+		pass
