ff4831a9d5657353724b32e8eb7e677974bf9287
Mostafa Rizk <mostafa.k.rizk@gmail.com>
2017-04-29 22:30:13 +1000

    Implemented first version of itillianise

--- tillybot.py
+++ tillybot.py
@@ -1,10 +1,29 @@
+import re
+
 class Tillybot():
 
 	def __init__(self):
-		pass
+		vocab_file = open('resources/vocabulary.txt')
+		vocabulary = vocab_file.read()
+		self.vocabulary = vocabulary.split()
 
 	def itillianise(self, message_text):
-		return text
+		word_list = re.sub("[^\w]", " ",  message_text).split()
+		message_text_new = ""
+		
+		for word in word_list:
+			try:
+				word_index = self.vocabulary.index(word)
+				word_replacement = self.vocabulary[word_index+1]
+			except ValueError:
+				word_replacement = word
+			message_text_new += word_replacement
+			
+			if word != word_list[len(word_list)-1]:
+				message_text_new += " "
+		
+		message_text_new += "."
+		return message_text_new
 
 	def has_keywords(self, message_text):
 		pass
